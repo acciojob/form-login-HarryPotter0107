ca494c89c3fc083f1e9b45fc80c1a68314b988d0
HarryPotter0107 <132452554+HarryPotter0107@users.noreply.github.com>
2024-02-27 01:29:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
+	let input = document.querySelectorAll("input")
+	let firstName = input[0].value
+	let lastname = input[1].value;
+	let fullName = firstName + " " + lastname;
+	alert(fullName)
 
 }
